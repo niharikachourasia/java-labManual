db8f72f8e1a1f19de60756b99839239ed32dc45b
nihchour <nihchour@DIN51003193.corp.capgemini.com>
2018-04-10 17:24:30 +0530

    lab-2

--- newproject/src/Emp2.java
+++ newproject/src/Emp2.java
@@ -1,9 +1,9 @@
-import java.util.Date;
+//import java.util.Date;
 import static java.lang.System.out;
 class Emp2{
 public static void main(String[] args){
 		System.out.println("The details of the employee are as follows:");
-		Date dt = new Date();
+		Date dt = new Date(01,01,2011);
 		//out.println("Current date: "+ dt);
 		
 		EmployeeDetail e = null;
@@ -14,23 +14,32 @@ public static void main(String[] args){
 		e = new EmployeeDetail("laveena", "agarwal", 500000, "a", dt);
 		//count=count+1;
 		e.displayDetails();
+		dt.displayDate();
 		
+		dt = new Date(12,01,2019);
 		e = new EmployeeDetail("nikki", "chourasia", 1900000, "a", dt);
 	//	count=count+1;
 		e.displayDetails();
+		dt.displayDate();
 		
+		
+		dt = new Date(13,01,2019);
 		e = new EmployeeDetail("teena", "bodhwani", 30000, "b", dt);
 		//count=count+1;
 		e.displayDetails();
+		dt.displayDate();
 		
+		dt = new Date(19,01,2019);
 		e = new EmployeeDetail("teena", "bodhwani", 30000, "b", dt);
 		//count=count+1;
 		e.displayDetails();
+		dt.displayDate();
 		
+		dt = new Date(18,01,2019);
 		e = new EmployeeDetail("teena", "bodhwani", 30000, "b", dt);
 		//count=count+1;
 		e.displayDetails();
-		
+		dt.displayDate();
 		//c.getInvoices();
 		
 	}
--- newproject/src/EmployeeDetail.java
+++ newproject/src/EmployeeDetail.java
@@ -7,7 +7,7 @@ import java.io.*;
 class EmployeeDetail{
 
 private String fName,lName,grade;
-Date dt;
+Date doj;
 private float salary;
 private static int count=0;
 private int id;
@@ -23,12 +23,12 @@ private int id;
 }
 */
 	
-	public EmployeeDetail(String fName, String lName, float salary, String grade, Date dt){
+	public EmployeeDetail(String fName, String lName, float salary, String grade, Date doj){
 	//	System.out.println("inside parameterized constructor");
 		this.fName = fName;
 		this.lName = lName;
 		this.grade = grade;
-		this.dt = dt;
+		this.doj = doj;
 		this.salary = salary;
 		id = ++count;
 		}
@@ -38,8 +38,21 @@ private int id;
 		System.out.print("\nLast Name: "+ lName);
 		System.out.print("\nSalary: "+ salary);
 		System.out.print("\ngrade: "+ grade);
-		System.out.print("\njoining month: "+ jMonth);
+	//	System.out.print("\njoining month: "+ dt);
 		System.out.print("\nEmp id: "+ count);
+	}
+}
+
+class Date{
+	int day, month, year;
+	Date(int day, int month, int year){
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public void displayDate(){
+		System.out.println("joining date: "+ day + "/" + month + "/" + year + "/" );
 	}
 }
 	
